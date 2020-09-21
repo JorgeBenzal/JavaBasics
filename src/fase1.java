2408bf8d3c412c127edd67a0736bfd57290adb2f
Jordi Benzal <jordi.benzal@gmail.com>
2020-09-21 18:43:28 +0200

    "Commit Fase1 IT Academy"

---
+++ src/fase1.java
@@ -0,0 +1,16 @@
+
+public class fase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nom = "Jordi";
+		String cognom1 = "Benzal";
+		String cognom2 = "Iñigo";
+		int dia = 21;
+		int mes = 9;
+		int any = 2020;
+		
+	}
+
+}
